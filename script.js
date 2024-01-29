d6839f4401e498b01d4d002fe3bda84cf266e1d2
worsleyj <josephworsley23@gmail.com>
2024-01-29 09:27:39 -0500

    delete button now correctly removes row, added index value for books

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const bookDialog = document.querySelector('.book-dialog')
 const openDialog = document.querySelector('.add-book')
 const closeDialog = document.querySelector('.cancel-dialog')
 const submitDialog = document.querySelector('.submit-book')
+let index = 0;
 
 openDialog.addEventListener('click', () => {
     bookDialog.showModal();
@@ -14,21 +15,23 @@ closeDialog.addEventListener('click', () => {
 })
 
 submitDialog.addEventListener('click', () => {
+    index++;
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
     const hasRead = document.querySelector('#hasRead').checked; // boolean value of html checkbox
-    const book = new Book(title, author, pages, hasRead);
+    const book = new Book(title, author, pages, hasRead, index);
 
     addToLibrary(book);
     displayBook(book);
 })
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, index) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.index = index;
     this.hasRead = function() {
         if (this.read == true) {
             return "Read";
@@ -54,15 +57,15 @@ function displayBook(book) {
     author.textContent = book.author;
 
     let pages = row.insertCell(2);
-    pages.textContent = book.pages
+    pages.textContent = book.index;
 
     let read = row.insertCell(3);
     read.textContent = book.hasRead();
     
     const btn = document.createElement('button')
-    btn.innerText = 'x'
+    btn.innerText = 'x';
     btn.addEventListener('click', () => {
-        libraryTable.deleteRow(0);
+        libraryTable.deleteRow(book.index+1);
     })
 
     let remove = row.insertCell(4);
@@ -77,13 +80,17 @@ function displayLibrary(library) {
     }
 }
 
-const book = new Book("Book", "Author", 100, false)
-const book2 = new Book("Book2", "Author", 100, true)
+const book = new Book("Book", "Author", 100, false, index)
+index++;
+const book2 = new Book("Book2", "Author", 100, true, index)
+index++;
+const book3 = new Book("Book3", "Author", 100, true, index)
+index++;
+const book4 = new Book("Book4", "Author", 100, true, index)
+
 
 addToLibrary(book)
-addToLibrary(book)
-addToLibrary(book)
-addToLibrary(book)
-addToLibrary(book)
 addToLibrary(book2)
+addToLibrary(book3)
+addToLibrary(book4)
 displayLibrary(odinLibrary)
