1d35f4899b809306cd219565839c4b2a62742fdb
worsleyj <josephworsley23@gmail.com>
2024-01-29 09:17:33 -0500

    delete buttons added for book objects

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ submitDialog.addEventListener('click', () => {
     const pages = document.querySelector('#pages').value;
     const hasRead = document.querySelector('#hasRead').checked; // boolean value of html checkbox
     const book = new Book(title, author, pages, hasRead);
-    
+
     addToLibrary(book);
     displayBook(book);
 })
@@ -59,8 +59,14 @@ function displayBook(book) {
     let read = row.insertCell(3);
     read.textContent = book.hasRead();
     
-    // let remove = row.insertCell(4);
-    // remove.textContent = "REMOVE"
+    const btn = document.createElement('button')
+    btn.innerText = 'x'
+    btn.addEventListener('click', () => {
+        libraryTable.deleteRow(0);
+    })
+
+    let remove = row.insertCell(4);
+    remove.appendChild(btn)
 
 }
 
@@ -75,6 +81,9 @@ const book = new Book("Book", "Author", 100, false)
 const book2 = new Book("Book2", "Author", 100, true)
 
 addToLibrary(book)
-addToLibrary(book2)
 addToLibrary(book)
+addToLibrary(book)
+addToLibrary(book)
+addToLibrary(book)
+addToLibrary(book2)
 displayLibrary(odinLibrary)
