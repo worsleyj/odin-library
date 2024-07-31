2e70fd0e802a285e94551c748c154b0d22305244
worsleyj <josephworsley23@gmail.com>
2024-07-31 14:23:49 -0400

    add new book button

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ button.addEventListener("click", () => {
     modal.showModal();
 })
 
+const submitBtn = document.querySelector(".submit");
+submitBtn.addEventListener("click", () => {
+    const newTitle = document.getElementById("new-title")
+    const newAuthor = document.getElementById("new-author");
+    const newPages = document.getElementById("new-pages");
+    const newReadStatus = document.querySelector("#new-read-status").checked;
+    const newBook = new Book(newTitle.value, newAuthor.value, newPages.value, newReadStatus)
+    
+    addBookToLibrary(newBook);
+    displayBook(newBook);
+})
+
 function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
@@ -21,28 +33,29 @@ function addBookToLibrary(book) {
     library.push(book);
 }
 
-function displayLibrary() {
+function displayBook(book) {
     const libraryContainer = document.querySelector(".library-container");
-    library.forEach((book) => {
-        const bookCard = document.createElement("div");
-        const titleAuthor = document.createElement("div");
-        const pageNumber = document.createElement("div");
-        const readStatus = document.createElement("div");
+    const bookCard = document.createElement("div");
+    const titleAuthor = document.createElement("div");
+    const pageNumber = document.createElement("div");
+    const readStatus = document.createElement("div");
 
-        bookCard.setAttribute("class", "book-card");
-        titleAuthor.setAttribute("class", "title-author");
-        pageNumber.setAttribute("class", "page-number");
-        readStatus.setAttribute("class", "read-status");
+    bookCard.setAttribute("class", "book-card");
+    titleAuthor.setAttribute("class", "title-author");
+    pageNumber.setAttribute("class", "page-number");
+    readStatus.setAttribute("class", "read-status");
 
-        titleAuthor.textContent = book.title + " by " + book.author;
-        pageNumber.textContent = book.pages;
-        readStatus.textContent = book.read;
+    titleAuthor.textContent = book.title + " by " + book.author;
+    pageNumber.textContent = book.pages + " pages";
+    readStatus.textContent = book.read;
 
-        bookCard.appendChild(titleAuthor);
-        bookCard.appendChild(pageNumber);
-        bookCard.appendChild(readStatus);
-        libraryContainer.prepend(bookCard);
-    });
+    bookCard.appendChild(titleAuthor);
+    bookCard.appendChild(pageNumber);
+    bookCard.appendChild(readStatus);
+    libraryContainer.prepend(bookCard);
+}
+function displayLibrary() {
+    library.forEach((book) => {displayBook(book)});
 }
 
 const bookOne = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
