9755bafbdda193905dad3291bbc2124b015ec835
worsleyj <josephworsley23@gmail.com>
2024-07-31 12:23:17 -0400

    add book object and add-to-library function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("test");
+const library = [];
+
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    if (hasRead) {
+        this.readStatus = "has been read";
+    } else {
+        this.readStatus = "not read yet";
+    }
+}
+
+function addBookToLibrary(book) {
+    library.push(book);
+}
+
+const bookOne = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+console.log(bookOne);
+
+addBookToLibrary(bookOne);
+console.log(library[0].title);
